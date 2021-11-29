0f233f8ea535b82bfbe185a37d966fadd9a8fdca
ValarValar <dan4eg98@mail.ru>
2021-11-30 02:42:19 +0300

    7. More heroku deploy fixes

--- Tinder/settings.py
+++ Tinder/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aklyeb!18)ja43)-r%6l=#vliz5#d((tsw+)rr92-(jsvo(105'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -69,6 +69,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Tinder.urls'
+ALLOWED_HOSTS = ['*']
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 TEMPLATES = [
     {
