e35c40e277a36021b5c355715b7c4100be006fcc
ValarValar <dan4eg98@mail.ru>
2021-11-30 13:14:14 +0300

    7.Changing settings for deploy

--- Tinder/settings.py
+++ Tinder/settings.py
@@ -12,21 +12,25 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aklyeb!18)ja43)-r%6l=#vliz5#d((tsw+)rr92-(jsvo(105'
+
+#SECRET_KEY = 'django-insecure-aklyeb!18)ja43)-r%6l=#vliz5#d((tsw+)rr92-(jsvo(105'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-aklyeb!18)ja43)-r%6l=#vliz5#d((tsw+)rr92-(jsvo(105')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = []
-
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 # Application definition
 
@@ -60,6 +64,7 @@ AUTH_USER_MODEL = 'clientsApp.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -70,6 +75,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Tinder.urls'
 ALLOWED_HOSTS = ['*']
+
 
 
 
@@ -103,6 +109,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -140,10 +148,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
